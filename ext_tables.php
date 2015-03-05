18efd843f61acdded3f84424d65adb85890be0df
jonathanheilmann <mail@jonathan-heilmann.de>
2015-03-05 14:25:32 +0100

    Added palette for images

--- ext_tables.php
+++ ext_tables.php
@@ -38,4 +38,10 @@ if (TYPO3_MODE == 'BE') {
 	}
 }
 
+// Add special mfp palette
+$GLOBALS['TCA']['sys_file_reference']['palettes']['mfpPalette'] = array(
+	'showitem' => 'title, alternative;;;;3-3-3,--linebreak--, description',
+	'canNotCollapse' => TRUE
+);
+
 ?>
