a3e1fa5919726fbc2a0086699db077e527209563
Jonathan Heilmann <mail@jonathan-heilmann.de>
2017-10-26 17:45:16 +0200

    [TASK] Code clean up

--- Classes/Controller/MagnificpopupController.php
+++ Classes/Controller/MagnificpopupController.php
@@ -27,6 +27,7 @@ namespace JonathanHeilmann\JhMagnificpopup\Controller;
  ***************************************************************/
 
 use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Resource\FileRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -81,14 +82,15 @@ class MagnificpopupController extends ActionController
         $this->data = $this->cObj->data;
 
         // Get localized record
-        $localizedRecord = $this->pageRepository->getRecordOverlay('tt_content', $this->data, $GLOBALS['TSFE']->sys_language_uid, $GLOBALS['TSFE']->sys_language_mode);
-        if ($localizedRecord !== false && isset($localizedRecord['_LOCALIZED_UID']))
+        $localizedRecord = $this->pageRepository->getRecordOverlay('tt_content', $this->data,
+            $GLOBALS['TSFE']->sys_language_uid, $GLOBALS['TSFE']->sys_language_mode);
+        if ($localizedRecord !== false && isset($localizedRecord['_LOCALIZED_UID'])) {
             $this->data = $localizedRecord;
+        }
 
         $viewAssign['uid'] = $this->data['uid'];
 
-        switch ($this->settings['contenttype'])
-        {
+        switch ($this->settings['contenttype']) {
             case 'iframe':
                 $viewAssign = $this->iframe() + $viewAssign;
                 break;
@@ -97,28 +99,27 @@ class MagnificpopupController extends ActionController
                 if (
                     ($this->settings['content']['procedure_reference'] == 'ajax'
                         && !empty($this->settings['contenttype'])
-                    ) || $this->settings['content']['procedure_inline'] == 'ajax')
-                {
+                    ) || $this->settings['content']['procedure_inline'] == 'ajax') {
                     $viewAssign = $this->ajax() + $viewAssign;
                 } elseif (
                     ($this->settings['content']['procedure_reference'] == 'inline'
                         && !empty($this->settings['contenttype'])
-                    ) || $this->settings['content']['procedure_inline'] == 'inline')
-                {
+                    ) || $this->settings['content']['procedure_inline'] == 'inline') {
                     $viewAssign = $this->inline() + $viewAssign;
-                } elseif ($this->settings['content']['procedure_reference'] == '' && $this->settings['content']['procedure_inline'] == '')
-                {
+                } elseif ($this->settings['content']['procedure_reference'] == '' && $this->settings['content']['procedure_inline'] == '') {
                     // Add error if no method (inline or ajax) has been selected
-                    $this->addFlashMessage('Please select the method (inline or ajax) to display Magnific Popup content', 'Select method', AbstractMessage::WARNING);
-                } elseif ($this->settings['content']['procedure_reference'] != '' && empty($this->settings['contenttype']))
-                {
+                    $this->addFlashMessage('Please select the method (inline or ajax) to display Magnific Popup content',
+                        'Select method', AbstractMessage::WARNING);
+                } elseif ($this->settings['content']['procedure_reference'] != '' && empty($this->settings['contenttype'])) {
                     // Add error if no content has been selected
-                    $this->addFlashMessage('Please select a content to display with Magnific Popup', 'Select content', AbstractMessage::WARNING);
+                    $this->addFlashMessage('Please select a content to display with Magnific Popup', 'Select content',
+                        AbstractMessage::WARNING);
                 }
                 break;
             default:
                 // Add error if no "Display type" has been selected
-                $this->addFlashMessage('Please select a "Display type" to use Magnific Popup', 'Select "Display type"', AbstractMessage::WARNING);
+                $this->addFlashMessage('Please select a "Display type" to use Magnific Popup', 'Select "Display type"',
+                    AbstractMessage::WARNING);
         }
 
         // Signal for show action (may be used to modify the array assigned to fluid-template)
@@ -131,6 +132,7 @@ class MagnificpopupController extends ActionController
                 'viewAssign' => &$viewAssign
             )
         );
+
         // Assign array to fluid-template
         $this->view->assignMultiple($viewAssign);
     }
@@ -142,30 +144,30 @@ class MagnificpopupController extends ActionController
     {
         $viewAssign['type'] = 'ajax';
         // Use ajax procedure
-        if ($this->settings['contenttype'] == 'reference')
-        {
+        if ($this->settings['contenttype'] == 'reference') {
             // Get the list of pid's
             $uidArray = explode(',', $this->settings['content']['reference']);
             $pidInList = array();
-            foreach ($uidArray as $uid)
-            {
-                $row = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('pid', 'tt_content', 'uid='.$uid);
+            foreach ($uidArray as $uid) {
+                $row = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('pid', 'tt_content', 'uid=' . $uid);
                 $pidInList[] = $row['pid'];
             }
             // Configure the link
             $linkconf['parameter'] = $this->data['pid'];
-            $linkconf['additionalParams'] = '&' . ($this->settings['useEidForAjaxMethod'] != 1 ? 'type=109' : 'eID=jh_magnificpopup_ajax') .
-                '&jh_magnificpopup[type]=reference&jh_magnificpopup[uid]='.$this->settings['content']['reference'].'&jh_magnificpopup[pid]='.implode(',', $pidInList);
-        } else
-        {
+            $linkconf['additionalParams'] = '&' .
+                ($this->settings['useEidForAjaxMethod'] != 1 ? 'type=109' : 'eID=jh_magnificpopup_ajax') .
+                '&jh_magnificpopup[type]=reference&jh_magnificpopup[uid]=' . $this->settings['content']['reference'] .
+                '&jh_magnificpopup[pid]=' . implode(',', $pidInList);
+        } else {
             // Configure the link
             $linkconf['parameter'] = $this->data['pid'];
-            $linkconf['additionalParams'] = '&' . ($this->settings['useEidForAjaxMethod'] != 1 ? 'type=109' : 'eID=jh_magnificpopup_ajax') .
-                '&jh_magnificpopup[type]=inline&jh_magnificpopup[irre_parrentid]='.$this->data['uid'];
+            $linkconf['additionalParams'] = '&' .
+                ($this->settings['useEidForAjaxMethod'] != 1 ? 'type=109' : 'eID=jh_magnificpopup_ajax') .
+                '&jh_magnificpopup[type]=inline&jh_magnificpopup[irre_parrentid]=' . $this->data['uid'];
         }
         // Link-setup
         $lConf = array();
-        $lConf['ATagParams'] = 'class="mfp-ajax-'.$this->data['uid'].'"';
+        $lConf['ATagParams'] = 'class="mfp-ajax-' . $this->data['uid'] . '"';
         $lConf['parameter'] = $linkconf['parameter'];
         $lConf['additionalParams'] = $linkconf['additionalParams'];
         // Support old way of link-setup. Will be removed later!
@@ -173,10 +175,11 @@ class MagnificpopupController extends ActionController
         $viewAssign['link'] = $this->cObj->typoLink_URL($linkconf);
         $viewAssign['link-text'] = $this->settings['mfpOption']['text'];
 
-        if ($this->settings['linktype'] == 'file')
+        if ($this->settings['linktype'] == 'file') {
             ArrayUtility::mergeRecursiveWithOverrule($viewAssign, $this->renderLinktypeFile($lConf));
-        else
+        } else {
             $viewAssign['tsLink'] = $this->cObj->typoLink($this->settings['mfpOption']['text'], $lConf);
+        }
 
         // Get settings from flexform
         $this->getSettingsFromFlexform('ajax');
@@ -231,17 +234,18 @@ class MagnificpopupController extends ActionController
         $viewAssign['inlinecontent_id'] = 'mfp-inline-' . $this->data['uid'];
 
         // Link-setup
-        $lConf['ATagParams'] = 'class="mfp-inline-'.$this->data['uid'].'" data-mfp-src="#mfp-inline-'.$this->data['uid'].'"';
+        $lConf['ATagParams'] = 'class="mfp-inline-' . $this->data['uid'] . '" data-mfp-src="#mfp-inline-' . $this->data['uid'] . '"';
         $lConf['parameter'] = $GLOBALS['TSFE']->id;
         // Support old way of link-setup. Will be removed later!
         $viewAssign['link-class'] = 'mfp-inline-'.$this->data['uid'];
         $viewAssign['link'] = '#mfp-inline-'.$this->data['uid'];
         $viewAssign['link-text'] = $this->settings['mfpOption']['text'];
 
-        if ($this->settings['linktype'] == 'file')
+        if ($this->settings['linktype'] == 'file') {
             ArrayUtility::mergeRecursiveWithOverrule($viewAssign, $this->renderLinktypeFile($lConf));
-        else
+        } else {
             $viewAssign['tsLink'] = $this->cObj->typoLink($this->settings['mfpOption']['text'], $lConf);
+        }
 
         // Get settings from flexform
         $this->getSettingsFromFlexform('inline');
@@ -268,10 +272,11 @@ class MagnificpopupController extends ActionController
             $viewAssign['link-text'] = $this->settings['mfpOption']['text'];
         }
 
-        if ($this->settings['linktype'] == 'file')
+        if ($this->settings['linktype'] == 'file') {
             ArrayUtility::mergeRecursiveWithOverrule($viewAssign, $this->renderLinktypeFile($lConf));
-        else
+        } else {
             $viewAssign['tsLink'] = $this->cObj->typoLink($this->settings['mfpOption']['text'], $lConf);
+        }
 
         // Get settings from flexform
         $this->getSettingsFromFlexform('iframe');
@@ -292,15 +297,16 @@ class MagnificpopupController extends ActionController
         // Modify parameter to add jQuery selector class to link
         $parameter = $this->settings['mfpOption']['href'];
         $parameters = GeneralUtility::unQuoteFilenames($parameter, true);
-        if (count($parameters) >= 3)
-        {
+        if (count($parameters) >= 3) {
             $parameters[2] = $parameters[2] . ' ' . $selectorClass;
             // Quote values (has been unquoted by GeneralUtility::unQuoteFilenames)
-            foreach ($parameters as $key => $value)
+            foreach ($parameters as $key => $value) {
                 $parameters[$key] = '"' . $value . '"';
+            }
             $parameter = implode(' ', $parameters);
-        } else
+        } else {
             $lConf['ATagParams'] = 'class="' . $selectorClass . '"';
+        }
 
         $lConf['parameter'] = $parameter;
 
@@ -318,12 +324,15 @@ class MagnificpopupController extends ActionController
         $viewAssign = array();
 
         // Get file
-        /** @var \TYPO3\CMS\Core\Resource\FileRepository $fileRepository */
-        $fileRepository = $this->objectManager->get('TYPO3\\CMS\\Core\\Resource\\FileRepository');
-        $fileObjects = $fileRepository->findByRelation('tt_content', 'mfp_image', isset($this->data['_LOCALIZED_UID']) ? $this->data['_LOCALIZED_UID'] : $this->data['uid']);
+        /** @var FileRepository $fileRepository */
+        $fileRepository = $this->objectManager->get(FileRepository::class);
+        $fileObjects = $fileRepository->findByRelation(
+            'tt_content',
+            'mfp_image',
+            isset($this->data['_LOCALIZED_UID']) ? $this->data['_LOCALIZED_UID'] : $this->data['uid']
+        );
 
-        if (!empty($fileObjects))
-        {
+        if (!empty($fileObjects)) {
             /** @var \TYPO3\CMS\Core\Resource\FileReference $file */
             $file = $fileObjects[0];
             $this->cObj->setCurrentFile($file->getOriginalFile());
@@ -332,18 +341,19 @@ class MagnificpopupController extends ActionController
             $imageConf['file'] = $file;
             $imageConf['altText'] = $file->getProperty('alternative');
             $imageConf['titleText'] = $file->getProperty('title');
-            if (isset($this->settings['mfpOption']['file_width']) && !empty($this->settings['mfpOption']['file_width']))
+            if (isset($this->settings['mfpOption']['file_width']) && !empty($this->settings['mfpOption']['file_width'])) {
                 $imageConf["file."]["maxW"] = $this->settings['mfpOption']['file_width'];
+            }
 
-            if (isset($this->settings['mfpOption']['file_height']) && !empty($this->settings['mfpOption']['file_height']))
+            if (isset($this->settings['mfpOption']['file_height']) && !empty($this->settings['mfpOption']['file_height'])) {
                 $imageConf["file."]["maxH"] = $this->settings['mfpOption']['file_height'];
+            }
 
             // Render image
             $theImgCode = $this->cObj->cObjGetSingle('IMAGE', $imageConf);
 
             // Get image orientation
-            switch ($this->settings['mfpOption']['file_orient'])
-            {
+            switch ($this->settings['mfpOption']['file_orient']) {
                 case 1:
                     $viewAssign['imageorient'] = 'right';
                     break;
@@ -359,8 +369,9 @@ class MagnificpopupController extends ActionController
 
             // Render typolink
             $viewAssign['tsLink'] = $this->cObj->typoLink($theImgCode, $lConf);
-        } else
+        } else {
             $this->addFlashMessage('Please select an image', 'No image', AbstractMessage::WARNING);
+        }
 
         return $viewAssign;
     }
@@ -374,8 +385,10 @@ class MagnificpopupController extends ActionController
      */
     protected function getSettingsFromFlexform($forType)
     {
-        foreach ($this->settings['mfpOption'] as $key => $value)
-            if ($value != -1 && !empty($value))
-                $this->settings['type'][$forType][$key] = $value == 'local' ? $this->settings['mfpOption'][$key.'_local'] : $value;
+        foreach ($this->settings['mfpOption'] as $key => $value) {
+            if ($value != -1 && !empty($value)) {
+                $this->settings['type'][$forType][$key] = $value == 'local' ? $this->settings['mfpOption'][$key . '_local'] : $value;
+            }
+        }
     }
 }
